236a51a1a07490f3dde5dacf5281906729997297
Larry <manpower0708@gmail.com>
2020-12-20 13:23:59 +0800

    完成下拉選單的相關動畫及js

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -4,6 +4,12 @@ $(document).ready(function () {
   // 漢堡選單點後下拉選單出現
   $('.navBar__burger').click(function (e) {
     e.preventDefault();
-    $('.navBar__burger').toggleClass('navBar__burger--open');
+    $('.navBar__burger').toggleClass('is-open');
+    $('.navBar').toggleClass('is-show');
+    $('.navBar__nav__list--featureMb').removeClass('is-open');
+  });
+  $('.--feature').click(function (e) {
+    e.preventDefault();
+    $('.navBar__nav__list--featureMb').toggleClass('is-open');
   });
 });

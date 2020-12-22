59db6899e9b0d47efe3943ff608f01253c402976
Larry <manpower0708@gmail.com>
2020-12-22 18:41:05 +0800

    finish index.html and scroll animation, but there are some bug at bropdownMb

--- js/main.js
+++ js/main.js
@@ -10,7 +10,30 @@ $(document).ready(function () {
 
     $('.navBar__nav__list--show').click(function (e) { 
         e.preventDefault();
-        $('.navBar__nav__list--dropdownMb').toggleClass('is-open');
+        $('.navBar__nav__list--dropdownMb').addClass('is-open');
     });
+
+// ! 滑動完後重新點burgerMenu又會跳出來dropdownMb
+    $("#btn-card,#btnPad-card").on("click", function(){
+        $('html, body').animate({
+        scrollTop: $(".tagCard").offset().top
+        }, 750); 
+        $('.navBar__burger').toggleClass('is-open');
+        $('.navBar').toggleClass('is-show');
+     });
+     $("#btn-news,#btnPad-news").on("click", function(){
+        $('html, body').animate({
+        scrollTop: $(".tagNews").offset().top
+        }, 750); 
+        $('.navBar__burger').toggleClass('is-open');
+        $('.navBar').toggleClass('is-show');
+     });
+     $("#btn-booking,#btnPad-booking").on("click", function(){
+        $('html, body').animate({
+        scrollTop: $(".tagBooking").offset().top
+        }, 750); 
+        $('.navBar__burger').toggleClass('is-open');
+        $('.navBar').toggleClass('is-show');
+     });
 
 }); 
